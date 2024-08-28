f0b1bbdc45b22b1ec35e95f6773e4c27bb6a286f
Brendow271 <pashashkuropat@yandex.ru>
2024-08-28 21:22:28 +0300

    Created basic layout HomePage
    - Created home page
    - Created CategoryInfo.vue component
    - Updated classes

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <Navbar/>
+  <CategoryInfo/>
+</template>
+
+<script>
+import Navbar from '../components/MainNavbar.vue'
+import CategoryInfo from "../components/CategoryInfo.vue";
+export default {
+  components: {CategoryInfo, Navbar },
+  name: 'HomePage'
+}
+</script>
+
