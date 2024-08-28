9f7892bdec1557580088b29b0b095dcfc7b20631
Brendow271 <pashashkuropat@yandex.ru>
2024-08-28 18:06:56 +0300

    Created MainNavbar.vue component

---
+++ frontend/src/components/MainNavbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-app-bar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-btn text icon>
+        <v-icon>mdi-view-list</v-icon>
+      </v-btn>
+      <v-btn text>
+        Каталог
+      </v-btn>
+
+      <v-spacer/>
+
+      <v-btn text icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+      <v-btn text icon>
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+      <v-btn text>
+        Выход
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'MainNavbar',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
