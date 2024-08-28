cd70e457bac18a69064211b54143090a2327311b
Brendow271 <pashashkuropat@yandex.ru>
2024-08-28 19:06:44 +0300

    Created basic layout

--- frontend/src/views/ProductListing.vue
+++ frontend/src/views/ProductListing.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>Product</div>
+  <Navbar/>
+  <div>
+    <ProductCard/>
+  </div>
 </template>
 
 <script>
+  import Navbar from '../components/MainNavbar.vue'
+  import ProductCard from "../components/ProductCard.vue";
   export default {
+    components: {ProductCard, Navbar },
     name: 'ProductListing'
   }
 
