ae3518022a2d1e3e73d1aeb84e1f161ae24b492a
Dvimin <dmitriev.michail@mail.ru>
2024-09-02 14:24:23 +0300

    Created main page
    - Created the project architecture
    - Сreated a test controller

---
+++ backend/src/main/java/com/wirewave/wirewave/WirewaveApplication.java
@@ -0,0 +1,13 @@
+package com.wirewave.wirewave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirewaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WirewaveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wirewave/wirewave/WirewaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirewave.wirewave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirewaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
