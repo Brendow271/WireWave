8449cb5e896f23e20371e51c36f563b205208a15
Dvimin <dmitriev.michail@mail.ru>
2024-09-16 13:21:47 +0300

    Created classes for CategoriesSubcategories entity: - Entity: CategoriesSubcategories - Repository: CategoriesSubcategoriesRepository - Service: CategoriesSubcategoriesService

---
+++ backend/src/main/java/com/wirewave/wirewave/repository/CategoriesSubcategoriesRepository.java
@@ -0,0 +1,7 @@
+package com.wirewave.wirewave.repository;
+
+import com.wirewave.wirewave.entity.CategoriesSubcategories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriesSubcategoriesRepository extends JpaRepository<CategoriesSubcategories, Integer> {
+}
