ee12e9cf51cbd15f86254e72bac6198ba587eb60
Dvimin <dmitriev.michail@mail.ru>
2024-09-16 13:22:13 +0300

    Created classes for Category entity: - Entity: Category - Repository: CategoryRepository - Service: CategoryService

---
+++ backend/src/main/java/com/wirewave/wirewave/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.wirewave.wirewave.repository;
+
+import com.wirewave.wirewave.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
