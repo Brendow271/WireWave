ae7f73d650cacc45e3b285ea4b9e4cc76f26a84b
Dvimin <dmitriev.michail@mail.ru>
2025-02-18 15:17:54 +0300

    Added user controller and service with tests

--- backend/src/main/java/com/wirewave/wirewave/controller/UserController.java
+++ backend/src/main/java/com/wirewave/wirewave/controller/UserController.java
@@ -5,11 +5,19 @@ import com.wirewave.wirewave.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,7 +36,7 @@ public class UserController {
             return ResponseEntity.badRequest().body(null);
         }
 
-        user.setPassword(user.getHashedPassword(), passwordEncoder);
+        user.setPassword(user.getPassword(), passwordEncoder);
         User savedUser = userService.saveUser(user);
         return ResponseEntity.ok(savedUser);
     }
